fae5a053ee8d4767498f3976f633536e6a320ee2
yanzhenxing <zhenxing.yan@duobei.com>
2021-08-26 11:40:35 +0800

    recent files

---
+++ mongo/test.py
@@ -0,0 +1,88 @@
+import requests
+import unittest
+from client import conn
+
+class Test(unittest.TestCase):
+    def testGms(self):
+        url = "http://localhost:8889/gms/v1/collect-event/"
+        log = {
+            "platform": "native",
+            "UserAgent": "okhttp/4.9.0",
+            "result": 1,
+            "sdkVer": "1.5.0",
+            "uid": "gms1402205437565136897",
+            "channelId": "",
+            "os": "Android/8.1.0",
+            "type": "LeaveChannel",
+            "timestamp": 1629163654339,
+            "elapsed": 74,
+            "deviceId": "e7a39215dcd92061",
+            "channelSessionId": "",
+            "appId": "GaLknQbqNqe9p23z",
+            "errorCode": 0,
+            "appIdEncrypt": False,
+            "clientSessionId": "c926bb0d-af4e-4dff-b9b5-8624e4bfeaa7",
+            "Geo": {
+                "Isp": "中国联通",
+                "City": "北京市",
+                "Ip": "111.206.85.4",
+                "Country": "中国",
+                "Province": "北京市"
+            }
+        }
+        res = requests.post(url=url, data=log)
+        print(res)
+
+    def test_mongo(self):
+        col = conn["test"]["duobei"]
+        # data = {
+        #    "author": "abc123",
+        #    "title": "zzz",
+        #    "tags": ["programming", "database", "mongodb", "dada" ]
+        # }
+        # col.save(data)
+        res = col.aggregate([
+            {
+                "$group": {
+                   "_id": "$author",
+                    "tags": {
+                        "$push": "$tags"
+                    }
+                },
+            },
+            {"$addFields": {
+                "tags": {
+                    "$reduce": {
+                        "input": "$tags",
+                        "initialValue": [],
+                        "in": {"$concatArrays": ["$$value", "$$this"]}
+                    }
+                }
+            }},
+            {
+                "$project": {
+                    "_id": 0,
+                    "tags": 1,
+                }
+            }]
+        )
+        for i in res:
+            print(i)
+
+    def test_mongo2(self):
+        col = conn["test"]["duobei"]
+        res = col.aggregate([
+            {
+                "$group": {
+                    "_id": "$author",
+                    "title": {
+                        "$push": "$title"
+                    }
+                },
+            },
+        ])
+
+        for i in res:
+            print(i)
+
+
