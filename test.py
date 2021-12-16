8685e064ab6b65a7dc9a0aead78ed6979ee552ff
yanzhenxing123 <209243446@qq.com>
2021-12-16 23:40:58 +0800

    demo

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
 """
-this is is  a test by yanzx 
 """
+
 def main():
-    for i in range(10000000):
+    for i in range(100 ** 5):
         print(i)
 
 if __name__ == '__main__':
-    main()    
-
+    main()
