228011d69dd044d2b33b4897a8e17fffe6ccf23f
yanzhenxing123 <209243446@qq.com>
2023-07-21 15:17:21 +0800

    feat: test.py

--- test.py
+++ test.py
@@ -3,13 +3,23 @@ this is a test py
 """
 
 
-def main():
-    print("this is a test")
+import torch as t
+from torch.autograd import Variable as v
 
 
-def test():
-    print("I am test")
+# simple gradient
+a = v(t.FloatTensor([2, 3]), requires_grad=True)
+b = a + 3
+c = b * b * 3
+out = c.mean()
+out.backward()
+print('*'*10)
+print('=====simple gradient======')
+print('input')
+print(a.data)
+# print('compute result is')
+# print(out.data[0])
+print('input gradients are')
+print(a.grad.data)
 
 
-if __name__ == '__main__':
-    main()
