46a76971187ddff8789f1be24f8c84cd07182e04
yanzhenxing123 <209243446@qq.com>
2024-08-21 11:27:44 +0800

    feat: jump

---
+++ hot100/贪心算法/045. 跳跃游戏 II.py
@@ -0,0 +1,48 @@
+"""
+@Time: 2024/8/21 10:47
+@Author: yanzx
+@Desc:
+
+给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+
+每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+
+0 <= j <= nums[i]
+i + j < n
+返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+
+输入: nums = [2,3,1,1,4]
+输出: 2
+解释: 跳到最后一个位置的最小跳跃数是 2。
+     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+"""
+from typing import List
+
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1:
+            return 0
+        length, right_most = len(nums), 0
+
+        i = 0
+        count = 0
+        while i < len(nums) - 1:
+            # 1. 跳
+            right_most = i + nums[i]
+            count += 1
+            if right_most >= length - 1:  # 终止条件
+                return count
+            # 2. 看后边的更新 i
+            for j in range(i + 1, i + nums[i] + 1):
+                if right_most <= j + nums[j]:
+                    i = j
+                    right_most = j + nums[j]
+        return count
+
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [2, 3, 1, 1, 4]
+    res = s.jump(nums)
+    print(res)
---
+++ hot100/贪心算法/055. 跳跃游戏.py
@@ -0,0 +1,58 @@
+"""
+@Time: 2024/8/21 10:17
+@Author: yanzx
+@Desc:
+给你一个非负整数数组 nums ，你最初位于数组的 第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+"""
+
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return True
+        length, right_most = len(nums), 0
+        for i in range(len(nums)):
+            if i <= right_most:
+                right_most = max(right_most, i + nums[i])
+                if right_most >= length - 1:
+                    return True
+            else:
+                break
+        return False
+
+    def canJump1(self, nums: List[int]) -> bool:
+        """
+        贪心算法，后边跳的总长度>=现在的总长度，就更新
+        :param nums:
+        :return:
+        """
+        if len(nums) <= 1:
+            return True
+
+        i = 0
+        while i < len(nums):
+            cur = nums[i]
+            cur_jump = i + nums[i]
+            if cur_jump >= len(nums) - 1:  # 满足条件
+                return True
+
+            for j in range(i + 1, i + cur + 1):  # 查看后边的长度
+                next_jump = j + nums[j]
+                if next_jump >= cur_jump:  # 更新
+                    cur_jump = next_jump
+                    i = j
+            if cur == 0:
+                break
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    nums = [2, 3, 1, 1, 4]
+    nums = [3, 2, 1, 0, 4]
+    res = s.canJump(nums)
+    print(res)
