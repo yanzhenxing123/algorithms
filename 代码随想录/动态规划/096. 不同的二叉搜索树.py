12d27f22528d940bf6e87788176d1c9b3fca7c67
yanzhenxing123 <209243446@qq.com>
2024-08-24 15:16:00 +0800

    feat: a

---
+++ 代码随想录/动态规划/096. 不同的二叉搜索树.py
@@ -0,0 +1,14 @@
+"""
+@Time: 2024/8/24 15:08
+@Author: yanzx
+@Desc: 
+"""
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    s = Solution()
