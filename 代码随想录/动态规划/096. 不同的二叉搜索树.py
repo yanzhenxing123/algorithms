929a6194672a6b46dd14ee87d5a79e917dc41ab8
yanzhenxing123 <209243446@qq.com>
2024-08-25 10:15:25 +0800

    feat: a

--- 代码随想录/动态规划/096. 不同的二叉搜索树.py
+++ 代码随想录/动态规划/096. 不同的二叉搜索树.py
@@ -7,8 +7,24 @@
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        pass
+        """
+        1. dp[i] 代表n = i时有多少种不同的BST情况
+        2. dp[i] = dp[0]dp[i-1] + dp[1]dp[i-2] + ... + dp[i-1]dp[0]
+        3. 初始化长度：n+1，且dp[0]=dp[1]=1
+        :param n:
+        :return:
+        """
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(0, i):
+                dp[i] = dp[j] * dp[i - 1 - j] + dp[i]
+
+        print(dp)
+        return dp[-1]
 
 
 if __name__ == '__main__':
     s = Solution()
+    s.numTrees(10)
--- 代码随想录/动态规划/096.不同的二叉搜索树.py
+++
@@ -1,5 +0,0 @@
-"""
-@Time: 2024/8/22 15:49
-@Author: yanzx
-@Desc: 
-"""
