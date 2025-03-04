0b10e016d31dc3659822fd8d9f83cc2b9a31b941
yanzhenxing <yanzhenxing@bytedance.com>
2025-03-04 11:32:08 +0800

    feat: 最长递增子序列

---
+++ hot100/10-动规-300-最长递增子序列.py
@@ -0,0 +1,60 @@
+"""
+@Author: yanzx
+@Date: 2025/3/4 10:57
+@Description:
+
+300. 最长递增子序列
+已解答
+中等
+相关标签
+相关企业
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+
+子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+
+
+示例 1：
+
+输入：nums = [10,9,2,5,3,7,101,18]
+输出：4
+解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+示例 2：
+
+输入：nums = [0,1,0,3,2,3]
+输出：4
+示例 3：
+
+输入：nums = [7,7,7,7,7,7,7]
+输出：1
+
+
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        两个循环，检查当前数字是否比之前的数字大，然后
+        :param nums:
+        :return:
+        """
+        if not nums:
+            return 0
+        dp = [1] * len(nums)
+        res = 1
+        for i in range(1, len(nums)):
+            for j in range(0, i + 1):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+            res = max(dp[i], res)
+        return res
+
+
+if __name__ == '__main__':
+    nums = [0, 1, 0, 3, 2, 3]
+    s = Solution()
+    res = s.lengthOfLIS(nums)
+    print(res)
---
+++ hot100/10-动规-322-零钱兑换.py
@@ -0,0 +1,54 @@
+"""
+@Author: yanzx
+@Date: 2025/3/4 10:30
+@Description:
+
+给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+计算并返回可以凑成总金额所需的最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+你可以认为每种硬币的数量是无限的。
+
+
+
+示例 1：
+
+输入：coins = [1, 2, 5], amount = 11
+输出：3
+解释：11 = 5 + 5 + 1
+
+
+示例 2：
+输入：coins = [2], amount = 3
+输出：-1
+
+示例 3：
+输入：coins = [1], amount = 0
+输出：0
+
+
+其实就是背包问题，无限次的拿，问装满这个背包最少需要多少件物品？
+
+dp[j]: 表示装满容量为j的背包需要的最少物品的数量
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # 先遍历物品，再遍历背包
+        dp = [float("inf")] * (amount + 1)
+        dp[0] = 0
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] = min(dp[j], dp[j - coin] + 1)
+                print(dp)
+        return dp[amount] if dp[amount] != float('inf') else -1
+
+
+if __name__ == '__main__':
+    coins = [1, 2, 5]
+    amount = 11
+    s = Solution()
+    res = s.coinChange(coins, amount)
+    print(res)
