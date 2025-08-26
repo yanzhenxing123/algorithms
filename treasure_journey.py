e322ba7c5654d94f50e6f6074ddf851a3535b7b7
yanzhenxing123 <209243446@qq.com>
2025-08-26 19:53:32 +0800

    a

--- treasure_journey.py
+++ treasure_journey.py
@@ -10,34 +10,29 @@ def solve() -> None:
     out_lines = []
     for _ in range(t):
         n = int(input().strip())
-        a = [[0] * n for _ in range(n)]
-        for i in range(n):
-            row = list(map(int, input().split()))
-            for j in range(n):
-                a[i][j] = row[j]
+        a = [list(map(int, input().split())) for _ in range(n)]
 
         prefix = [[0] * (n + 1) for _ in range(n)]
         row_sum = [0] * n
         for i in range(n):
             ps = 0
+            pref_i = prefix[i]
+            row_i = a[i]
             for j in range(n):
-                ps += a[i][j]
-                prefix[i][j + 1] = ps
+                ps += row_i[j]
+                pref_i[j + 1] = ps
             row_sum[i] = ps
 
         ans = 0
+        pref_row1 = prefix[0]
         for c in range(1, n + 1):
-            # 在第一行，只能在到达列 c 之前经过房间的对外门
-            base = prefix[0][c - 1]
-
+            base = pref_row1[c - 1]
             for i in range(n):
-                row_idx = i
-                if (i + 1) % 2 == 1:
-                    # 奇数行向右：经过 c..n 的对外门
-                    tail = row_sum[row_idx] - prefix[row_idx][c - 1]
+                pref_i = prefix[i]
+                if (i + 1) & 1:
+                    tail = row_sum[i] - pref_i[c - 1]
                 else:
-                    # 偶数行向左：经过 1..c 的对外门
-                    tail = prefix[row_idx][c]
+                    tail = pref_i[c]
                 total = base + tail
                 if total > ans:
                     ans = total
