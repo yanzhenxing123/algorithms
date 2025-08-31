336262f616640cc4ecf628e42addda049570b0eb
yanzhenxing123 <209243446@qq.com>
2025-08-31 16:57:16 +0800

    feat: all

--- 秋招笔试/拼多多/04.py
+++ 秋招笔试/拼多多/04.py
@@ -1,84 +1,50 @@
-"""
-@Time: 2025/8/31 16:45
-@Author: yanzx
-@Description:
-
-
-对于每组测试数据，在单独的一行里输出所需的最少操作数。
-
-示例1
-
-输入：
-3
-3 8
-3 5 1
-2 5 1
-3 8
-2 5 1
-5 2 1
-3 8
-2 5 2
-5 2 1
-
-
-输出：
-0
-1
-2
-
-"""
-
-
-
 import sys
-input = sys.stdin.readline
+read = sys.stdin.readline
 
-class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n + 1))
-        self.size = [1] * (n + 1)
+class UnionFind:
+    def __init__(self, size):
+        self.fa = list(range(size + 1))
+        self.cnt = [1] * (size + 1)
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+    def get(self, x):
+        if self.fa[x] != x:
+            self.fa[x] = self.get(self.fa[x])
+        return self.fa[x]
 
-    def union(self, x, y):
-        fx, fy = self.find(x), self.find(y)
-        if fx != fy:
-            if self.size[fx] < self.size[fy]:
-                fx, fy = fy, fx
-            self.parent[fy] = fx
-            self.size[fx] += self.size[fy]
+    def merge(self, u, v):
+        fu, fv = self.get(u), self.get(v)
+        if fu != fv:
+            if self.cnt[fu] < self.cnt[fv]:
+                fu, fv = fv, fu
+            self.fa[fv] = fu
+            self.cnt[fu] += self.cnt[fv]
 
-def solve():
-    T = int(input().strip())
-    for _ in range(T):
-        n, m = map(int, input().split())
-        a = list(map(int, input().split()))
-        b = list(map(int, input().split()))
+def main():
+    t = int(read().strip())
+    for _ in range(t):
+        n, m = map(int, read().split())
+        arr1 = list(map(int, read().split()))
+        arr2 = list(map(int, read().split()))
 
-        dsu = DSU(m)
-        involved = set()
+        uf = UnionFind(m)
+        used = set()
 
         for i in range(n):
-            if a[i] != b[i]:
-                dsu.union(a[i], b[i])
-                involved.add(a[i])
-                involved.add(b[i])
+            if arr1[i] != arr2[i]:
+                uf.merge(arr1[i], arr2[i])
+                used.add(arr1[i])
+                used.add(arr2[i])
 
-        comp = {}
-        for x in involved:
-            root = dsu.find(x)
-            comp.setdefault(root, []).append(x)
+        groups = {}
+        for val in used:
+            root = uf.get(val)
+            groups.setdefault(root, []).append(val)
 
-        ans = 0
-        for nodes in comp.values():
-            # 真正需要的操作次数是 (len(nodes) - 已经自然匹配的边数)
-            # 这里简化：一个分量的最少操作次数 = len(nodes) - 1
-            ans += len(nodes) - 1
+        res = 0
+        for g in groups.values():
+            res += len(g) - 1
 
-        print(ans)
+        print(res)
 
 if __name__ == "__main__":
-    solve()
+    main()
