5a0ab8bb48a1004eb6144975e448b169b78a09e2
yanzhenxing.7 <yanzhenxing.7@jd.com>
2025-09-08 15:25:27 +0800

    feat: all

--- DL/09.layerNorm.py
+++ DL/09.layerNorm.py
@@ -52,9 +52,6 @@ class RMSNorm(nn.Module):
         self.gamma = nn.Parameter(torch.ones(hidden_size))
         self.eps = eps
 
-
-
-
     def forward(self, x):
         """
         torch.Size([4, 3, 16])
@@ -62,11 +59,32 @@ class RMSNorm(nn.Module):
         :return:
         """
         x_2 = torch.pow(x, 2)
-        x_2_mean = torch.mean(x_2, dim=-1, keepdim=True) #
+        x_2_mean = torch.mean(x_2, dim=-1, keepdim=True)  #
         x_norm = x / torch.sqrt(x_2_mean + self.eps)
 
         output = self.gamma * x_norm
         return output
+
+
+class GroupNorm(nn.Module):
+    def __init__(self, num_features, num_groups=32, eps=1e-5):
+        super(GroupNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(1, num_features, 1, 1))
+        self.bias = nn.Parameter(torch.zeros(1, num_features, 1, 1))
+        self.num_groups = num_groups
+        self.eps = eps
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        G = self.num_groups
+        assert C % G == 0
+
+        x = x.view(N, G, -1)
+        mean = x.mean(-1, keepdim=True)
+        var = x.var(-1, keepdim=True)
+
+        x = (x - mean) / (var + self.eps).sqrt()
+        x = x.view(N, C, H, W)
 
 
 if __name__ == '__main__':
@@ -79,4 +97,3 @@ if __name__ == '__main__':
     rms_norm_layer = RMSNorm(emb_dim)
     res = rms_norm_layer(x)
     print(res)
-
